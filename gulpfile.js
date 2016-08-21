e6b50c691b5e06308898e51124a0a696b87355ad
pensart <guy.pensart@hotmail.com>
2016-08-22 01:23:37 +0200

    gulpfile vars and directory changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,28 @@
+// -- Settings
+
+var set = {
+    src: 'src',
+    dist: 'dist',
+    styles: 'styles',
+};
+
+// -- requirements
+
 var gulp = require('gulp'),
-    plumber = require('gulp-plumber');
-var sass = require('gulp-sass');
+    plumber = require('gulp-plumber'),
+    sass = require('gulp-sass');
 
 gulp.task('styles', function(){
-  gulp.src(['src/styles/scss/main.scss'])
+  gulp.src( set.src + '/' + set.styles + '/**/*.scss')
     .pipe(plumber({
       errorHandler: function (error) {
         console.log(error.message);
         this.emit('end');
     }}))
     .pipe(sass())
-    .pipe(gulp.dest('dist/styles/'))
+    .pipe( gulp.dest( set.dist + '/' + set.styles ))
 });
 
 gulp.task('default', function(){
-  gulp.watch("src/styles/**/*.scss", ['styles']);
+  gulp.watch( set.src + '/' + set.styles + '/**/*.scss', ['styles']);
 });
