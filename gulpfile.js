b9dbf7d35a01537d84e74855bdf50e74f0f4891e
pensart <guy.pensart@hotmail.com>
2016-08-22 05:05:58 +0200

    autoprefixer last 2 versions fails on windows

--- gulpfile.js
+++ gulpfile.js
@@ -10,9 +10,9 @@ var set = {
 
 var gulp = require('gulp'),
     plumber =       require('gulp-plumber'),
+    autoPrefixer =  require('gulp-autoprefixer'),
     sass =          require('gulp-sass'),
-    sourceMaps =    require('gulp-sourcemaps'),
-    autoPrefixer =  require('gulp-autoprefixer');
+    sourceMaps =    require('gulp-sourcemaps');
 
 gulp.task('styles', function () {
     gulp.src(set.src + '/' + set.styles + '/**/*.scss')
@@ -24,7 +24,8 @@ gulp.task('styles', function () {
     }))
     .pipe(sourceMaps.init())
     .pipe(sass())
-    .pipe(autoPrefixer('last 2 versions'))
+    // last 2 versions fails with autoprefixer on windows10
+    .pipe(autoPrefixer())
     .pipe(sourceMaps.write())
     .pipe(gulp.dest(set.dist + '/' + set.styles))
 });
